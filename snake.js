daa084a93f2db3f7283584c7196e02a665357af9
wadziha <63168293+wadziha@users.noreply.github.com>
2022-01-27 18:12:59 +0100

    Update snake.js

--- snake.js
+++ snake.js
@@ -1,44 +1,39 @@
-
-let direction = {x: 0, y: 0}; 
-let speed_s = 1;
-let lastPaintTime = 0;
-let snake_position = [{x: 10, y: 10}
+let vstupni_souradnice = {x: 0, y: 0};
+let rychlost_hada = 10;
+let skore = 0;
+let cas_posledni_vykresleni = 0;
+let snake_pozice = [{x: 19, y: 11}];
 ];
 food_segment={x: 13, y: 14};
-//Functions
-function main(consttime) {
+function main(aktualni_cas) {
     window.requestAnimationFrame(main);
-    console.log(consttime);
-    if((consttime - lastPaintTime)/1000 < 1/speed_s){
+    // console.log(aktualni_cas)
+    if ((aktualni_cas - cas_posledni_vykresleni) / 1000 < 1 / rychlost_hada) {
         return;
     }
-   
-    lastPaintTime = consttime;
-  game_draw();
+    cas_posledni_vykresleni = aktualni_cas;
+    game_draw();
 }
+
 
 
 function game_draw(){
 
     // Show snake - Display
-    game_board.innerHTML = ''
-    snake_position.forEach(snake_segment =>{
-      const  snakeElement = document.createElement('div');
-        snakeElement.style.gridRowStart = snake_segment.y;
-        snakeElement.style.gridColumnStart = snake_segment.x;
+   //zobrazeni hada 
+    hraci_plocha.innerHTML = "";
+    snake_pozice.forEach((e, index) => {
+        snakeElement = document.createElement('div');
+        snakeElement.style.gridRowStart = e.y;
+        snakeElement.style.gridColumnStart = e.x;
         snakeElement.classList.add('snake');
-
-
-       game_board.appendChild(snakeElement);
-
-    })
-
-    const foodElement=dokument.createElement('div');
-    foodElement.style.gridRowStart = food_segment.y;
-    foodElement.style.gridColumnStart = food_segment.x;
-    foodElement.classList.add('food');
-   game_board.appendChild(foodElement);
-
-}
-
-window.requestAnimationFrame(main);
+        hraci_plocha.appendChild(snakeElement);
+    });
+    // zobrazeni jidla 
+    foodElement = document.createElement('div');
+    foodElement.style.gridRowStart = jidlo.y;
+    foodElement.style.gridColumnStart = jidlo.x;
+    foodElement.classList.add('jidlo')
+    hraci_plocha.appendChild(foodElement);
+    
+    window.requestAnimationFrame(main);
